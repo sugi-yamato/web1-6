cde531101adc9c77435eb18934f8781972a9b5ad
Unknown <g1727035@st.t-kougei.ac.jp>
2018-06-04 18:17:53 +0900

    猫

---
+++ Cat Escape/Assets/GameDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+	// Use this for initialization
+
+        GameObject hpGauge;
+
+	void Start () {
+        this.hpGauge = GameObject.Find("hpGauge");
+		
+	}
+	
+	// Update is called once per frame
+	public void DecreaseHp () {
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+	}
+}
